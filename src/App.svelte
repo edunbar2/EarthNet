c8d3f43c9659ae6302b66108ba03b7543981c9eb
edunbar <edunbar@unca.edu>
2023-02-20 11:20:52 -0500

    Inital work on dashboard.

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,22 @@
 <script>
- 
+ import Navbar from "./Components/Navbar.svelte";
+ import Header from "./Components/Header.svelte";
+ let tabs = ["Dashboard", "Configure"];
 </script>
 
+
+
+<Navbar {tabs}/>
 <main>
   <p>This is a temporary tag for effect</p>
 </main>
 
 <style>
-
+main{
+  background-color: #6B7361;
+  height: 95%;
+  width: 90%;
+  margin: 2.5% 10%;
+  position: absolute;
+}
 </style>
---
+++ src/Components/Navbar.svelte
@@ -0,0 +1,29 @@
+<script>
+export let tabs = [];
+</script>
+
+
+<main>
+<div class=bar>
+    <p>This is the navbar</p>
+    <p>This is the navbar</p>
+    <p>This is the navbar</p>
+    <p>This is the navbar</p>
+    <p>This is the navbar</p>
+    <p>This is the navbar</p>
+    <p>This is the navbar</p>
+</div>
+
+</main>
+
+<style>
+.bar{
+    position: fixed;
+    background-color: #7c8b69;
+    margin-top: 2.5%;
+    height: 95%;
+    width: 10%;
+    
+}
+
+</style>
